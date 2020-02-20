6b17137980257f01abccb0a84e014dc2da228185
oleksandr.akulenko <akula410@gmail.com>
2020-02-20 09:33:09 +0200

    Add example helper

---
+++ example/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/akula410/helper"
+)
+
+
+func main(){
+	fmt.Println("==============helper.Hash===============")
+	exampleHash("12345678")
+	fmt.Println("==================end===================")
+}
+
+func exampleHash(pass string){
+	fmt.Println("password: ", pass)
+	var hash1 = helper.Hash.GetHashBCrypt(pass)
+	var hash2 = helper.Hash.GetHashBCrypt(pass)
+
+	fmt.Println("Hash 1 for password: ", hash1)
+	fmt.Println("Hash 2 for password: ", hash2)
+
+	fmt.Println("Result hash 1 for password: ", helper.Hash.AskHashBCrypt(hash1, pass))
+	fmt.Println("Result hash 2 for password: ", helper.Hash.AskHashBCrypt(hash2, pass))
+}
